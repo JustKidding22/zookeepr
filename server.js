93f70f1ebf34dde6c6b37a62bba7f31200c9419d
James K <projdk90@gmail.com>
2021-10-19 20:22:00 -0700

    finishing touch front end functionality

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
@@ -12,6 +14,8 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
